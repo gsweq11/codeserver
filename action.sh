009ba998bdbb8ff27423e560d616e1e85900e93f
TeamKillerX <randigithub356@gmail.com>
2022-07-11 02:27:49 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=dev
 git clone -b $branch https://github.com/TeamKillerX/KillerX-Music /root/TeamKillerX
-cp killerx-music/.env /root/TeamKillerX/.env
+cp TeamKillerX/.env /root/TeamKillerX/.env
 cd /root/TeamKillerX
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t killerx-music
 docker run --privileged --env-file .env --rm -i killerx-music

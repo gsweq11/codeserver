5254e3f1bff6950afdfd140210bf91da4b96ad77
gsweq11 <109459448+gsweq11@users.noreply.github.com>
2022-07-18 12:02:00 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=master
-git clone -b $branch https://github.com/TeamKillerX/KillerX-Music /root/TeamKillerX
-cp TeamKillerX/.env /root/TeamKillerX/.env
-cd /root/TeamKillerX
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t killerx-music
-docker run --privileged --env-file .env --rm -i killerx-music
+git clone -b $branch https://github.com/gsweq11/YukkiMusicBot /root/YukkiMusicBot
+cp YukkiMusicBot/.env /root/YukkiMusicBot/.env
+cd /root/YukkiMusicBot
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t YukkiMusic
+docker run --privileged --env-file .env --rm -i YukkiMusic

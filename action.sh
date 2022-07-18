780334953d0864966c1d6a8e232e75b37553ea1d
gsweq11 <109459448+gsweq11@users.noreply.github.com>
2022-07-18 12:53:01 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=master
 git clone -b $branch https://github.com/gsweq11/yukkimusicbot /root/yukkimusic
 cp yukkimusic/.env /root/yukkimusic/.env
-cd /root/yukkimusicbot
+cd /root/yukkimusic
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t yukkimusic
 docker run --privileged --env-file .env --rm -i yukkimusic
